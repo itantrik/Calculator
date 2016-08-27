11bca3fc6024fb8b58065c74286707c6776cacaf
itantrik <s.gangwal@yahoo.in>
2016-08-27 11:28:32 +0530

    First Commit from the studio project

---
+++ app/src/androidTest/java/com/basiccalc/mybasiccalc/BasicCalCTest.java
@@ -0,0 +1,102 @@
+package com.basiccalc.mybasiccalc;
+
+import android.test.ActivityInstrumentationTestCase2;
+//import android.test.suitebuilder.annotation.LargeTest;
+//import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by SGANGWAL on 8/23/2016.
+ */
+public class BasicCalCTest extends ActivityInstrumentationTestCase2<BasicCalC> {
+
+    private Solo solo;
+
+    public BasicCalCTest() {
+        super(BasicCalC.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testForAddition() {
+        float firstNumber = 10;
+        float secondNumber = 20;
+
+        EditText firstEditText = (EditText) solo.getView(R.id.viewNum);
+        solo.enterText(firstEditText , String.valueOf(firstNumber));
+
+        EditText secondEditText = (EditText) solo.getView(R.id.viewNum2);
+        solo.enterText(secondEditText , String.valueOf(secondNumber));
+
+        float testResult = firstNumber + secondNumber;
+        solo.clickOnButton("ADD");
+
+        TextView output = (TextView)solo.getView(R.id.result);
+        assertEquals(String.valueOf(testResult),output.getText().toString());
+    }
+
+    public void testForSubtraction() {
+        float firstNumber = 10;
+        float secondNumber = 20;
+
+        EditText firstEditText = (EditText) solo.getView(R.id.viewNum);
+        solo.enterText(firstEditText , String.valueOf(firstNumber));
+
+        EditText secondEditText = (EditText) solo.getView(R.id.viewNum2);
+        solo.enterText(secondEditText , String.valueOf(secondNumber));
+
+        float testResult = firstNumber - secondNumber;
+        solo.clickOnButton("SUB");
+
+        TextView output = (TextView)solo.getView(R.id.result);
+        assertEquals(String.valueOf(testResult),output.getText().toString());
+
+    }
+
+    public void testForMultiplication() {
+        float firstNumber = 10;
+        float secondNumber = 20;
+
+        EditText firstEditText = (EditText) solo.getView(R.id.viewNum);
+        solo.enterText(firstEditText , String.valueOf(firstNumber));
+
+        EditText secondEditText = (EditText) solo.getView(R.id.viewNum2);
+        solo.enterText(secondEditText , String.valueOf(secondNumber));
+
+        float testResult = firstNumber * secondNumber;
+        solo.clickOnButton("MUL");
+
+        TextView output = (TextView)solo.getView(R.id.result);
+        assertEquals(String.valueOf(testResult),output.getText().toString());
+
+    }
+
+    public void testForDivision() {
+        //float firstNumber = 10;
+        //float secondNumber = 4;
+
+        EditText firstEditText = (EditText) solo.getView(R.id.viewNum);
+        solo.enterText(firstEditText , "10");
+
+        EditText secondEditText = (EditText) solo.getView(R.id.viewNum2);
+        solo.enterText(secondEditText , "20");
+
+        double testResult = 10.0 / 20.0;
+        solo.clickOnButton("DIV");
+
+        TextView output = (TextView)solo.getView(R.id.result);
+        assertEquals(String.valueOf(testResult),output.getText().toString());
+
+    }
+
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+}
---
+++ app/src/main/java/com/basiccalc/mybasiccalc/BasicCalC.java
@@ -0,0 +1,115 @@
+package com.basiccalc.mybasiccalc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.*;
+
+public class BasicCalC extends AppCompatActivity implements View.OnClickListener {
+
+    public Button btnAdd, btnSub, btnDiv, btnMul, btnRes, btnClear;
+    public TextView expression;
+    public EditText viewNum;
+    String num="0",exp="",functionResult="0";
+    int operator=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_calculator);
+        init();
+    }
+
+    private void init() {
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnRes = (Button) findViewById(R.id.btnRes);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        viewNum = (EditText) findViewById(R.id.viewNum);
+        expression = (TextView) findViewById(R.id.expression);
+
+    }
+
+    public void onClickNumber (View view) {
+
+    }
+
+    public void onClickOperator(View view) {
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                if(num==functionResult) {
+                    num = viewNum.getText().toString();
+                    exp = exp.concat(num);
+                }
+                exp=exp.concat(" + ");
+                num = viewNum.getText().toString();
+                viewNum.setText("");
+                operator=1;
+                break;
+            case R.id.btnSub:
+                if(num==functionResult) {
+                    num = viewNum.getText().toString();
+                    exp = exp.concat(num);
+                }
+                exp=exp.concat(" - ");
+                num = viewNum.getText().toString();
+                viewNum.setText("");
+                operator=2;
+                break;
+            case R.id.btnDiv:
+                if(num==functionResult) {
+                    num = viewNum.getText().toString();
+                    exp = exp.concat(num);
+                }
+                exp=exp.concat(" / ");
+                num = viewNum.getText().toString();
+                viewNum.setText("");
+                operator=4;
+                break;
+            case R.id.btnMul:
+                if(num==functionResult) {
+                    num = viewNum.getText().toString();
+                    exp = exp.concat(num);
+                }
+                exp=exp.concat(" x ");
+                num = viewNum.getText().toString();
+                viewNum.setText("");
+                operator=3;
+                break;
+            case R.id.btnRes:
+                functionResult = viewNum.getText().toString();
+                switch (operator) {
+                    case 1:
+                        exp=exp.concat(String.valueOf(functionResult));
+                        expression.setText(exp);
+                        functionResult = String.valueOf(Float.parseFloat(num) + Float.parseFloat(functionResult));
+                        break;
+                    case 2:
+                        exp=exp.concat(String.valueOf(functionResult));
+                        expression.setText(exp);
+                        functionResult = String.valueOf(Float.parseFloat(num) - Float.parseFloat(functionResult));
+                        break;
+                    case 3:
+                        exp=exp.concat(String.valueOf(functionResult));
+                        expression.setText(exp);
+                        functionResult = String.valueOf(Float.parseFloat(num) * Float.parseFloat(functionResult));
+                        break;
+                    case 4:
+                        exp=exp.concat(String.valueOf(functionResult));
+                        expression.setText(exp);
+                        functionResult = String.valueOf(Float.parseFloat(num) / Float.parseFloat(functionResult));
+                        break;
+                }
+                viewNum.setText(functionResult);
+                break;
+            case R.id.btnClear:
+                viewNum.setText("");
+                expression.setText("");
+                exp="";
+                num="0";
+                functionResult = "0";
+                break;
+        }
+    }
+}
